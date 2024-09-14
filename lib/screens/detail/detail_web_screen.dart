9eb0f0876bd1fe315685d950c2bff519a52602a4
atharianr <atharianrahmadani@gmail.com>
2024-09-14 17:27:01 +0700

    Fixing overflow

--- lib/screens/detail/detail_web_screen.dart
+++ lib/screens/detail/detail_web_screen.dart
@@ -20,18 +20,19 @@ class _DetailWebScreenState extends State<DetailWebScreen> {
     return Scaffold(
       body: Container(
         color: const Color(0xFF172336),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 64,
-          ),
-          child: Center(
-            child: SizedBox(
-              width: 1200,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
+        child: Center(
+          child: SizedBox(
+            width: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 16,
+                    left: 64,
+                    right: 64,
+                  ),
+                  child: Text(
                     "ValorantPEEK",
                     style: GoogleFonts.montserrat(
                       fontWeight: FontWeight.w700,
@@ -39,113 +40,129 @@ class _DetailWebScreenState extends State<DetailWebScreen> {
                       color: Colors.white,
                     ),
                   ),
-                  const SizedBox(height: 32),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.asset(widget.valorantAgent.image),
-                            ),
-                            const SizedBox(height: 16),
-                            Scrollbar(
-                              controller: _scrollController,
-                              child: Container(
-                                height: 108.0,
-                                padding: const EdgeInsets.only(bottom: 16),
-                                child: ListView(
-                                  controller: _scrollController,
-                                  scrollDirection: Axis.horizontal,
-                                  children:
-                                      widget.valorantAgent.skills.map((skill) {
-                                    return Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(10),
-                                        child: Container(
-                                          width: 150.0,
-                                          color: Colors.red.withOpacity(0.5),
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: <Widget>[
-                                              Image.network(
-                                                skill.skillImage,
-                                                width: 32.0,
-                                                height: 32.0,
-                                              ),
-                                              const SizedBox(height: 8.0),
-                                              Text(
-                                                skill.skillName,
-                                                style: GoogleFonts.montserrat(
-                                                  fontWeight: FontWeight.w700,
-                                                  fontSize: 14.0,
-                                                  color: Colors.white,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  }).toList(),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                ),
+                const SizedBox(height: 16),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        bottom: 16,
+                        left: 64,
+                        right: 64,
                       ),
-                      const SizedBox(width: 32),
-                      Expanded(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24.0),
-                          ),
-                          child: Container(
-                            padding: const EdgeInsets.all(16),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
                             child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    Text(
-                                      widget.valorantAgent.name,
-                                      style: GoogleFonts.montserrat(
-                                        fontWeight: FontWeight.w700,
-                                        fontSize: 24.0,
-                                      ),
-                                    ),
-                                    const FavoriteButton()
-                                  ],
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child:
+                                      Image.asset(widget.valorantAgent.image),
                                 ),
-                                Container(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 16.0,
-                                  ),
-                                  child: Text(
-                                    widget.valorantAgent.description,
-                                    style: GoogleFonts.montserrat(
-                                      fontWeight: FontWeight.w300,
-                                      fontSize: 14.0,
+                                const SizedBox(height: 16),
+                                Scrollbar(
+                                  controller: _scrollController,
+                                  child: Container(
+                                    height: 108.0,
+                                    padding: const EdgeInsets.only(bottom: 16),
+                                    child: ListView(
+                                      controller: _scrollController,
+                                      scrollDirection: Axis.horizontal,
+                                      children: widget.valorantAgent.skills
+                                          .map((skill) {
+                                        return Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            child: Container(
+                                              width: 150.0,
+                                              color:
+                                                  Colors.red.withOpacity(0.5),
+                                              child: Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: <Widget>[
+                                                  Image.network(
+                                                    skill.skillImage,
+                                                    width: 32.0,
+                                                    height: 32.0,
+                                                  ),
+                                                  const SizedBox(height: 8.0),
+                                                  Text(
+                                                    skill.skillName,
+                                                    style:
+                                                        GoogleFonts.montserrat(
+                                                      fontWeight:
+                                                          FontWeight.w700,
+                                                      fontSize: 14.0,
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      }).toList(),
                                     ),
                                   ),
                                 ),
                               ],
                             ),
                           ),
-                        ),
+                          const SizedBox(width: 32),
+                          Expanded(
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24.0),
+                              ),
+                              child: Container(
+                                padding: const EdgeInsets.all(16),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text(
+                                          widget.valorantAgent.name,
+                                          style: GoogleFonts.montserrat(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 24.0,
+                                          ),
+                                        ),
+                                        const FavoriteButton()
+                                      ],
+                                    ),
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 16.0,
+                                      ),
+                                      child: Text(
+                                        widget.valorantAgent.description,
+                                        style: GoogleFonts.montserrat(
+                                          fontWeight: FontWeight.w300,
+                                          fontSize: 14.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
